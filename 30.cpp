9aa07201a1682666a4106f1c14fdecd2e3b3e9f3
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-23 09:20:15 -0800

    Code Created

---
+++ 30.cpp
@@ -0,0 +1,8 @@
+#include<iostream>
+#include<bits/stdc++.h>
+int main()
+{
+	int i=20;
+	cout<<"%x"<<i; 
+	return 0;
+}
---
+++ 31.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+int main()
+{   
+	int a=10;
+	int b=20;
+	if(a=0)
+	{
+		cout<<b;
+	} 
+	else
+	{  
+		b=b*10;
+		cout<<b;
+	}
+	return 0;
+}
