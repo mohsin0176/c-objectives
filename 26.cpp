f8d071f7786e5ec38358c58d180679be6f66bcc2
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-23 08:43:42 -0800

    Code Created

---
+++ 26.cpp
@@ -0,0 +1,18 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std;
+
+int  main()
+{  
+	 
+    int a=10;
+    int b=10;
+    int c=20;
+    int d=30;
+
+    int s=((a=b)?c:d);
+    cout<<s;
+
+
+	return 0;
+}
