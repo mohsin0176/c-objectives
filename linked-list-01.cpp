9e02ff076d9550061b0dcc6280d372f6ef9e1317
mohsin0176 <mohsin2010ju@gmail.com>
2021-02-24 09:14:26 -0800

    Code Created

--- linked-list-01.cpp
+++ linked-list-01.cpp
@@ -9,3 +9,50 @@ struct Node
 	struct Node *prev;
 };
 
+void insert(struct Node **start,int data)
+{
+	if(*start==NULL)
+	{
+		struct Node *new_node=new Node();
+		new_node->data=data;
+		new_node->next=new_node->prev=new_node;
+		*start=new_node;
+		return;
+	} 
+
+	Node *last=(*start)->prev;
+	struct Node *new_node=new Node();
+	new_node->data=data;
+	new_node->next=*start;
+	(*start)->prev=new_node;
+	new_node->prev=last;
+	last->next=new_node;
+}
+
+void display(struct Node *start)
+{
+	struct Node *temp=start;
+
+	
+	while(temp->next!=start)
+	{
+		cout<<temp-data;
+		temp=temp->next;
+	};
+
+	cout<<temp->data;
+} 
+
+int main()
+{
+
+	struct Node *start=NULL;
+
+	insert(&start,4);
+	insert(&start,5);
+	insert(&start,10);
+	insert(&start,20); 
+
+	display(start);
+	return 0;
+}
